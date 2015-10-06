9d3886b7f95a160bdc678a0947413fafce290f4a
Mountainbrussells <mountainbrussells@gmail.com>
2015-10-06 15:04:26 -0600

    Assignment 14 completed

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -11,17 +11,37 @@
 @implementation StarTrekDictionaries
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-    /* WORK HERE */
-    return @"";
+    id favDrink = characterDictionary[@"favorite drink"];
+    if (favDrink != nil && [favDrink isKindOfClass:[NSString class]]) {
+        return favDrink;
+    }
+    return @"No favorite drink";
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
-    /* WORK HERE */
+    NSMutableArray *favDrinks = [[NSMutableArray alloc] init];
+    if (charactersArray != nil && [charactersArray[0] isKindOfClass:[NSDictionary class]]) {
+        for (NSDictionary *character in charactersArray) {
+            NSString *favDrink = character[@"favorite drink"];
+            [favDrinks addObject:favDrink];
+        }
+        // Returning a mutableArray works, I thought it might not.  Why?
+        return favDrinks;
+    }
+    
     return @[];
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-    /* WORK HERE */
+    if (characterDictionary != nil && [characterDictionary isKindOfClass:[NSDictionary class]]){
+        
+        NSMutableDictionary *mutableCharacter = [characterDictionary mutableCopy];
+            
+        [mutableCharacter setValue:@"Make it so" forKey:@"quote"];
+            
+        return mutableCharacter;
+        
+    }
     return @{};
 }
 
