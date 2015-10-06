19599bd875d322309df49b8b408ffa43f54e8bbf
Mountainbrussells <mountainbrussells@gmail.com>
2015-10-06 11:25:34 -0600

    Assignment 13 completed

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -11,22 +11,40 @@
 @implementation StarTrekArrays
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
-    /* WORK HERE */
-    return @[];
+    
+    NSArray *returnArray = [characterString componentsSeparatedByString:@";"];
+    
+    return returnArray;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @"";
+    NSString *returnString = [characterArray componentsJoinedByString:@";"];
+    return returnString;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @[];
+    NSMutableArray *returnArray = [[NSMutableArray alloc] initWithArray:characterArray];
+    
+    NSSortDescriptor *sortDesc = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+    [returnArray sortUsingDescriptors:@[sortDesc]];
+    
+    
+    return returnArray;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
-    /* WORK HERE */
+    
+    NSMutableArray *mutableCharacterArray = [[NSMutableArray alloc] initWithArray:characterArray];
+   
+    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'worf'"];
+    
+    [mutableCharacterArray filterUsingPredicate:containsWorf];
+    
+    if (mutableCharacterArray.count > 0) {
+        return  TRUE;
+    }
+    
+    
     return NO;
 }
 
