5aecc42a250df800c6eefc6802c7d6275770ee7a
Mountainbrussells <mountainbrussells@gmail.com>
2015-10-07 09:20:41 -0600

    Do with single while loop

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -14,20 +14,32 @@
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     NSMutableString *returnString = [NSMutableString stringWithFormat:@""];
     
-    if (number < otherNumber) {
+//    if (number < otherNumber) {
+//        for (NSInteger n = number; n <= otherNumber; n++) {
+//            NSMutableString *numberString = [NSMutableString stringWithFormat:@"%ld", (long)n];
+//            [returnString appendString:numberString];
+//        }
+//    } else if (number > otherNumber) {
+//        for (NSInteger n = otherNumber; n <= number; n++) {
+//            NSMutableString *numberString = [NSMutableString stringWithFormat:@"%ld", (long)n];
+//            [returnString appendString:numberString];
+//        }
+//    } else {
+//        returnString = [NSMutableString stringWithFormat:@"%ld", (long)number];
+//    }
+    
+    while (number <= otherNumber) {
         for (NSInteger n = number; n <= otherNumber; n++) {
             NSMutableString *numberString = [NSMutableString stringWithFormat:@"%ld", (long)n];
             [returnString appendString:numberString];
         }
-    } else if (number > otherNumber) {
-        for (NSInteger n = otherNumber; n <= number; n++) {
-            NSMutableString *numberString = [NSMutableString stringWithFormat:@"%ld", (long)n];
-            [returnString appendString:numberString];
-        }
-    } else {
-        returnString = [NSMutableString stringWithFormat:@"%ld", (long)number];
+        return returnString;
     }
     
+    for (NSInteger n = otherNumber; n <= number; n++) {
+        NSMutableString *numberString = [NSMutableString stringWithFormat:@"%ld", (long)n];
+        [returnString appendString:numberString];
+    }
     
     return returnString;
 }
